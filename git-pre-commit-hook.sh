b70c9f6844b379548a78431525382cea9d01f7b7
Philip Lijnzaad <plijnzaad@gmail.com>
2017-05-15 17:22:52 +0200

    added python check

--- git-pre-commit-hook.sh
+++ git-pre-commit-hook.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
-## simple example of pre-commit hook. Put this in
-## $YOURWORKINGCOPY/.git/hooks/pre-commit
+## simple example of a pre-commit hook. Put this in
+## $YOURWORKINGCOPY/.git/hooks/pre-commit. Note that this check script
+## itself is not versioned.
 
 readfirst=./env.sh.example
 if [ -f $readfirst ] ; then 
    source $readfirst
 fi
 
-## Perl:
+## python (simplistic; also check python compileall; PyChecker,
+## Pyflakes, Pylint)
+for file in  $(find . -name '*.py' -print); do
+    if python -m py_compile $file; then 
+        :
+    else
+        status=$?
+        echo "Problem with $file, exit code $status" >&2
+        exit $status
+    fi
+done
+
+## perl:
 for file in  $(find . -name '*.p[lm]' -print); do
     if perl -wc $file; then 
         :
@@ -30,3 +43,4 @@ for file in  $(find . -name '*.R' -print); do
         exit $status
     fi
 done
+
